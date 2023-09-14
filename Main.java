c73dd87cee67b91a87f33df9e25f4b8d32824919
M3rg <123715188+Mergezy@users.noreply.github.com>
2023-09-14 23:23:30 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int[] N_array = new int[scanner.nextInt()];
+        int N = 4;
+        int[] number = new int[N];
+
+        for (int i = 0; i < N_array.length; i++) {
+            N_array[i] = random.nextInt(N);
+            number[N_array[i]]++;
+        }
+
+        int maxID = 0;
+        for (int i = 0; i < N; i++) {
+            if (number[i] > number[maxID]) {
+                maxID = i;
+            }
+        }
+
+        System.out.println((number[maxID] > (N_array.length / 2)) ? maxID : -1);
+    }
+}
 
